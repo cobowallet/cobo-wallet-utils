cae03397ad1a4b359ce9730c42ae9da0c0a6f022
Aaron Chen <chen_dong85@foxmail.com>
2018-07-24 15:01:39 +0800

    add the code

---
+++ qrCode.js
@@ -0,0 +1,47 @@
+import md5 from 'crypto-js/md5'
+import { find, propEq } from 'ramda';
+
+const regexString = MaxStringLength => `.{1,${MaxStringLength}}`;
+
+export const splitData = (data, capacity) => {
+  if(typeof data !== 'string' || data.length <= 0 || capacity < 2) {
+    throw Error('input value is not supported')
+  }
+  const checkSum = md5(data).toString();
+  const dataList = data.match(new RegExp(regexString(capacity), 'g'));
+  const dataLength = dataList.length;
+  return dataList.map((code, index) => ({
+    total: dataLength,
+    index,
+    checkSum,
+    value: code,
+  }));
+};
+
+
+export const assembleData = (data, result = []) => {
+  const dataObject = JSON.parse(data);
+  if(checkResult(result, dataObject)) {
+    return result;
+  } else {
+    if (typeof find(propEq('index', dataObject.index))(result) === 'undefined') {
+      const next = [...result, dataObject];
+      if(checkResult(next, dataObject)){
+        return next
+      }
+      return next
+    }
+  }
+};
+
+const checkResult = (result, data) => {
+  if(result.length === data.total) {
+    const resultString = result.reduce((acc, current) => acc + current.value, '');
+    if(md5(resultString).toString() === data.checkSum) {
+      return true
+    } else {
+      throw Error('data is not matched')
+    }
+  }
+  return false
+};
---
+++ test/qrCode.test.js
@@ -0,0 +1,103 @@
+import { splitData, assembleData } from '../qrCode'
+
+describe('qrCode', function() {
+  describe('splitData', function() {
+    it('should split the Data into mutil parts', function() {
+      const data = '12345678';
+      const dataList = splitData(data, 4);
+      const result = [
+        {
+          total: 2,
+          index: 0,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '1234',
+        },
+        {
+          total: 2,
+          index: 1,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '5678',
+        },
+      ];
+
+      expect(dataList).toEqual(result)
+    });
+    it('should split the Data into one parts', function() {
+      const data = '12345678';
+      const dataList = splitData(data, 10);
+      const result = [
+        {
+          total: 1,
+          index: 0,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '12345678',
+        }
+      ];
+      expect(dataList).toEqual(result)
+    });
+    it('should throw error if the input value is not supported', function() {
+      const data = '';
+      expect(() => splitData(data, 1)).toThrow('input value is not supported');
+    });
+  });
+
+  describe('assembleData', function() {
+    it('should assemble the data', function() {
+      const dataOne = '{"total":2,"index":0,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"1234"}'
+      const dataTwo = '{"total":2,"index":1,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"5678"}'
+      let result = [];
+      result = assembleData(dataOne, result);
+      result = assembleData(dataTwo, result);
+      const exceptedResult = [
+        {
+          total: 2,
+          index: 0,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '1234',
+        },
+        {
+          total: 2,
+          index: 1,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '5678',
+        },
+      ]
+      expect(result).toEqual(exceptedResult)
+    });
+
+    it('should assemble the current data when the input data is more than required', function() {
+      const dataOne = '{"total":2,"index":0,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"1234"}';
+      const dataTwo = '{"total":2,"index":1,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"5678"}';
+      const dataThree = '{"total":2,"index":1,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"5678"}';
+      let result = [];
+      result = assembleData(dataOne, result);
+      result = assembleData(dataTwo, result);
+      result = assembleData(dataThree, result);
+      const exceptedResult = [
+        {
+          total: 2,
+          index: 0,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '1234',
+        },
+        {
+          total: 2,
+          index: 1,
+          checkSum:'25d55ad283aa400af464c76d713c07ad',
+          value: '5678',
+        },
+      ]
+      expect(result).toEqual(exceptedResult)
+    });
+
+    it('should raise error if the checksum is not matched', function() {
+      const dataOne = '{"total":2,"index":0,"checkSum":"25d55ad283aa400af464c76d713c07vd","value":"1234"}'
+      const dataTwo = '{"total":2,"index":1,"checkSum":"25d55ad283aa400af464c76d713c07vd","value":"5678"}'
+      let result = [];
+      expect(() => {
+        result = assembleData(dataOne, result);
+        result = assembleData(dataTwo, result);
+      }).toThrow('data is not matched')
+    });
+  });
+});
