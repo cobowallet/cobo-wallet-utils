f01e2bf6c5f240bdd6100586cf6829215472eb75
Aaron Chen <chen_dong85@foxmail.com>
2018-11-13 16:23:27 +0800

    compress and encode data, decompress and decode data

--- qrCode.js
+++ qrCode.js
@@ -1,25 +1,36 @@
 import md5 from 'crypto-js/md5'
-import { find, propEq } from 'ramda';
+import { find, propEq, sort } from 'ramda';
+import zlib from 'browserify-zlib';
+import { Buffer } from 'safe-buffer';
 
 const regexString = MaxStringLength => `.{1,${MaxStringLength}}`;
 
-export const splitData = (data, capacity) => {
+export const splitData = (data, capacity, compress=true, rate) => {
   if(typeof data !== 'string' || data.length <= 0 || capacity < 2) {
     throw Error('input value is not supported')
   }
-  const checkSum = md5(data).toString();
-  const partCount = Math.ceil(data.length / capacity)
-  const partLength = Math.ceil(data.length / partCount)
+  const maybeCompressedData = compress? zlib.gzipSync(data).toString('base64'): data;
+  const checkSum = md5(maybeCompressedData).toString();
+  const partCount = Math.ceil(maybeCompressedData.length / capacity)
+  const partLength = Math.ceil(maybeCompressedData.length / partCount)
   const parts = []
   for (let i = 0; i < partCount; i++) {
-    parts.push(data.substring(partLength * i, Math.min(partLength * i + partLength, data.length)))
+    parts.push(maybeCompressedData.substring(partLength * i, Math.min(partLength * i + partLength, maybeCompressedData.length)))
   }
-  return parts.map((code, index) => ({
-    total: partCount,
-    index,
-    checkSum,
-    value: code,
-  }));
+  return parts.map((code, index) => {
+    return index === 0 ?
+    {
+      total: partCount,
+      index,
+      checkSum,
+      value: code,
+      compress
+    }: {
+      total: partCount,
+      index,
+      value: code,
+    }
+  });
 };
 
 export const assembleData = (data, result = []) => {
@@ -28,6 +39,25 @@ export const assembleData = (data, result = []) => {
     return [...result, dataObject];
   }
   return result
+};
+
+const diff = (a, b) => a.index - b.index
+
+export const extractData = (data) => {
+  const received = sort(diff, data);
+  if(received.length <= 0 || !received[0].checkSum || typeof received[0].compress === 'undefined') {
+    throw Error('input value is not supported')
+  }
+  const { compress, checkSum } = received[0];
+  const maybeCompressedData = received.reduce((acc, current)=> acc + current.value, '');
+  console.log(maybeCompressedData);
+  if (verifyCheckSum(maybeCompressedData, checkSum)) {
+    console.log(zlib.gunzipSync(Buffer.from(maybeCompressedData,'base64')).toString());
+    return compress? zlib.gunzipSync(Buffer.from(maybeCompressedData,'base64')).toString() : maybeCompressedData;
+  }
+  else{
+    throw Error('checkSum not valid');
+  }
 };
 
 export const verifyCheckSum = (data, checkSum) => {
--- test/qrCode.test.js
+++ test/qrCode.test.js
@@ -1,99 +1,33 @@
-import { splitData, assembleData } from '../qrCode'
+import {splitData, assembleData, extractData} from '../qrCode'
+import zlib from 'browserify-zlib';
+import { Buffer } from 'safe-buffer';
 
 describe('qrCode', function() {
   describe('splitData', function() {
     it('should split the Data into mutil parts', function() {
       const data = '12345678';
-      const dataList = splitData(data, 4);
-      const result = [
-        {
-          total: 2,
-          index: 0,
-          checkSum:'25d55ad283aa400af464c76d713c07ad',
-          value: '1234',
-        },
-        {
-          total: 2,
-          index: 1,
-          checkSum:'25d55ad283aa400af464c76d713c07ad',
-          value: '5678',
-        },
-      ];
-      expect(dataList).toEqual(result)
+      const dataList = splitData(data, 4, true);
+      const result = [{"checkSum": "070ad62969ef903b5d39a979e61d40a0", "compress": true, "index": 0, "total": 10, "value": "H4sI"}, {"index": 1, "total": 10, "value": "AAAA"}, {"index": 2, "total": 10, "value": "AAAA"}, {"index": 3, "total": 10, "value": "AzM0"}, {"index": 4, "total": 10, "value": "MjYx"}, {"index": 5, "total": 10, "value": "NTO3"}, {"index": 6, "total": 10, "value": "AACv"}, {"index": 7, "total": 10, "value": "2uCa"}, {"index": 8, "total": 10, "value": "CAAA"}, {"index": 9, "total": 10, "value": "AA=="}]
+      expect(dataList).toEqual(result);
     });
     it('should split the Data into one parts', function() {
       const data = '12345678';
-      const dataList = splitData(data, 10);
+      const dataList = splitData(data, 10, false);
       const result = [
         {
           total: 1,
           index: 0,
           checkSum:'25d55ad283aa400af464c76d713c07ad',
           value: '12345678',
+          compress: false
         }
       ];
       expect(dataList).toEqual(result)
     });
-    it('should split the Data into one parts', function() {
-      const data = '123456789ABCDEFGHIJKLMN';
-      const dataList = splitData(data, 7);
-      const result = [
-        {
-          total: 4,
-          index: 0,
-          checkSum:'2935a382342ac8f5b5f0d9a78cfdc2de',
-          value: '123456',
-        },
-        {
-          total: 4,
-          index: 1,
-          checkSum:'2935a382342ac8f5b5f0d9a78cfdc2de',
-          value: '789ABC',
-        },
-        {
-          total: 4,
-          index: 2,
-          checkSum:'2935a382342ac8f5b5f0d9a78cfdc2de',
-          value: 'DEFGHI',
-        },
-        {
-          total: 4,
-          index: 3,
-          checkSum:'2935a382342ac8f5b5f0d9a78cfdc2de',
-          value: 'JKLMN',
-        }
-      ];
-      expect(dataList).toEqual(result)
-    });
-    it('should split the Data into mutil parts', function() {
+    it('should split the Chinese Character into mutil parts', function() {
       const data = '测试测试测试测试测试测';
       const dataList = splitData(data, 3);
-      const result = [
-        {
-          total: 4,
-          index: 0,
-          checkSum:'a1000e23ef4dfecb16fd241e6e28d229',
-          value: '测试测',
-        },
-        {
-          total: 4,
-          index: 1,
-          checkSum:'a1000e23ef4dfecb16fd241e6e28d229',
-          value: '试测试',
-        },
-        {
-          total: 4,
-          index: 2,
-          checkSum:'a1000e23ef4dfecb16fd241e6e28d229',
-          value: '测试测',
-        },
-        {
-          total: 4,
-          index: 3,
-          checkSum:'a1000e23ef4dfecb16fd241e6e28d229',
-          value: '试测',
-        },
-      ];
+      const result = [{"checkSum": "6f8c4a28dd7084fdf8336ff7b0ca95a0", "compress": true, "index": 0, "total": 14, "value": "H4s"}, {"index": 1, "total": 14, "value": "IAA"}, {"index": 2, "total": 14, "value": "AAA"}, {"index": 3, "total": 14, "value": "AAA"}, {"index": 4, "total": 14, "value": "A3u"}, {"index": 5, "total": 14, "value": "2tf"}, {"index": 6, "total": 14, "value": "vF+"}, {"index": 7, "total": 14, "value": "qnP"}, {"index": 8, "total": 14, "value": "cJM"}, {"index": 9, "total": 14, "value": "A4k"}, {"index": 10, "total": 14, "value": "Bsc"}, {"index": 11, "total": 14, "value": "iEA"}, {"index": 12, "total": 14, "value": "AAA"}, {"index": 13, "total": 14, "value": "="}]
       expect(dataList).toEqual(result)
     });
     it('should throw error if the input value is not supported', function() {
@@ -104,8 +38,8 @@ describe('qrCode', function() {
 
   describe('assembleData', function() {
     it('should assemble the data', function() {
-      const dataOne = '{"total":2,"index":0,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"1234"}'
-      const dataTwo = '{"total":2,"index":1,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"5678"}'
+      const dataOne = '{"total":2,"index":0,"checkSum":"25d55ad283aa400af464c76d713c07ad","value":"1234", "compress": true}';
+      const dataTwo = '{"total":2,"index":1,"value":"5678"}';
       let result = [];
       result = assembleData(dataOne, result);
       result = assembleData(dataTwo, result);
@@ -115,11 +49,11 @@ describe('qrCode', function() {
           index: 0,
           checkSum:'25d55ad283aa400af464c76d713c07ad',
           value: '1234',
+          compress: true
         },
         {
           total: 2,
           index: 1,
-          checkSum:'25d55ad283aa400af464c76d713c07ad',
           value: '5678',
         },
       ]
@@ -151,4 +85,19 @@ describe('qrCode', function() {
     });
 
   });
+  describe('extractData', function () {
+    it('should throw Error when data is invalid', function () {
+      const data = {};
+      expect(() => extractData(data)).toThrow('input value is not supported');
+    });
+    it('should throw Error when checkSum not match', function() {
+      const splitedData = [{"checkSum": "123", "compress": true, "index": 0, "total": 10, "value": "H4sI"}, {"index": 1, "total": 10, "value": "AAAA"}, {"index": 2, "total": 10, "value": "AAAA"}, {"index": 3, "total": 10, "value": "AzM0"}, {"index": 4, "total": 10, "value": "MjYx"}, {"index": 5, "total": 10, "value": "NTO3"}, {"index": 6, "total": 10, "value": "AACv"}, {"index": 7, "total": 10, "value": "2uCa"}, {"index": 8, "total": 10, "value": "CAAA"}, {"index": 9, "total": 10, "value": "AA=="}]
+      expect(() => extractData(splitedData)).toThrow('checkSum not valid');
+    })
+    it('should return base64 decoded and extracted data', function () {
+      const splitedData = [{"checkSum": "070ad62969ef903b5d39a979e61d40a0", "compress": true, "index": 0, "total": 10, "value": "H4sI"}, {"index": 1, "total": 10, "value": "AAAA"}, {"index": 2, "total": 10, "value": "AAAA"}, {"index": 3, "total": 10, "value": "AzM0"}, {"index": 4, "total": 10, "value": "MjYx"}, {"index": 5, "total": 10, "value": "NTO3"}, {"index": 6, "total": 10, "value": "AACv"}, {"index": 7, "total": 10, "value": "2uCa"}, {"index": 8, "total": 10, "value": "CAAA"}, {"index": 9, "total": 10, "value": "AA=="}]
+      const result = extractData(splitedData);
+      expect(result).toEqual('12345678');
+    })
+  })
 });
